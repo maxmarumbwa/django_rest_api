3c3cb8b1a9e996d923373ac317a849ae1d7c5d5a
Farai Marumbwa <maxmarumbwa@gmail.com>
2025-06-01 17:48:40 +0200

    throttle rates applied

--- mainapp/settings.py
+++ mainapp/settings.py
@@ -131,4 +131,12 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.BasicAuthentication",
     ],
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "1/day",
+        "user": "2/day",
+    },
 }
